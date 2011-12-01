8851f39dd366f253c7dc9805fe45466634eecc5b
ryan1_00 <ryan1_00@bd1813ac-cada-4670-a6aa-601ec87a8edc>
2011-12-01 12:23:46 +0000

    Added CDP Format to CDPTypeList.h Added More CDP Types to CDPTypeList.h Added Capabilities to CDPTypeList.h (need to discover 0x80 & 0x0400 seem to be IPPhone related) Added Macros to check Capability Masks in CDPTypeList.h Added ctoui to utility.h (converts u_char* to u_int) Added Capabilities to debug output

--- trunk/src/utility.h
+++ trunk/src/utility.h
@@ -1,9 +1,30 @@
 #ifndef __UTILITY_H_
 #define __UTILITY_H_
 
+#include <string>
+
 inline std::wstring ctow(const char* src)
 {
     return std::wstring(src, src + strlen(src));
+}
+
+inline u_int ctoui(const u_char* src, bool net_host)
+{
+	u_int *y = 0;
+	u_short a = 0;
+	u_short b = 0;
+	u_int z = 0;
+
+	y = (u_int *) src; // read 4 Bytes
+	if (net_host) {
+		a = ntohs(*((u_short *) y)); // Tweak 1st 2 Bytes
+		b = ntohs(*((u_short *) y+1)); // Tweak 2nd 2 Bytes
+		z = (a + b);
+	} else {
+		z = *y;
+	}
+	
+    return z;
 }
 
 #endif
